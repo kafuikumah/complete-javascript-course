c211e2915d05f458051b1a70d6b00c4c0e88c804
kafuikumah <kafui.kofi.kumah@gmail.com>
2023-05-30 23:45:30 +0000

    Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,5 +1,6 @@
 "use strict";
-//
+// ACTIVATING STRICT MODE
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
@@ -8,3 +9,26 @@ if (hasDriversLicense) console.log("I can dfive! D");
 
 const interface = "Audio";
 const private = 345543;
+*/
+// FUNCTION
+
+function logger() {
+  console.log("Ny name is Kadui");
+}
+
+//Calling, running or invoking the function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(12, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(12, 15);
+console.log(appleOrangeJuice);
